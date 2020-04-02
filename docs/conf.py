c6ce716cba08c983235f16e43c2bb8e7df5bbe95
Reinoud Sleeman <reino@Reinouds-MacBook-Pro.local>
2020-04-02 15:58:03 +0200

    add

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+html_theme = 'nature'
+
 master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
