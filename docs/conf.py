d32e9e18cbf49e0afe1b3370580647e931d98cff
Reinoud Sleeman <reino@Reinouds-MacBook-Pro.local>
2020-04-02 16:05:32 +0200

    add

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import os
-html_theme = 'nature'
 
 master_doc = 'index'
 
@@ -50,6 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
